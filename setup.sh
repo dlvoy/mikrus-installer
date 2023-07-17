0f72aeb99bd5e242eef5bfd07b77d99e26c6fb56
Dominik Dzienia <dominik.dzienia@gmail.com>
2023-07-17 12:41:56 +0000

    Querying for API key

--- setup.sh
+++ setup.sh
@@ -12,10 +12,20 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 # MAIN SCRIPT
 #=======================================
 
-setup_update_repo
-check_git
-check_docker
-check_docker_compose
-check_jq
-setup_packages
-setup_node
+
+#setup_update_repo
+#check_git
+#check_docker
+#check_docker_compose
+#check_jq
+#check_dotenv
+#setup_packages
+#setup_node
+
+whiptail --title "Witamy" --yesno "Ten skrypt zainstaluje Nightscout na bieżącym serwerze mikr.us\n\nJeśli na tym serwerze istnieje już instalacja Nightscout - ten skrypt spróbuje ją przekonfigurować" --yes-button "$uni_start" --no-button "$uni_exit" 12 70 
+exit_on_no_cancel
+
+prompt_mikrus_host
+prompt_mikrus_apikey
+
+whiptail --msgbox "Gotowe!" 5 12
